9aa8204d28a3c0782e6dd18342ba6dcbc4d450c6
eediallo <elhhabdouldiallo@gmail.com>
2024-10-28 18:29:03 +0000

    makes enemy moves both direction

--- js/updateEnemyPosition.js
+++ js/updateEnemyPosition.js
@@ -4,9 +4,18 @@ import { enemyContainer } from "./config.js";
 
 //===============update======================
 export function updateEnemyPosition() {
-    newEnemy.x++;
-    enemyElement.style.left = `${newEnemy.x}px`;
-    if (newEnemy.x >= enemyContainer.width) {
-      newEnemy.x = 0;
+    if (newEnemy.direction === 'left') {
+        newEnemy.x--;
+    } else {
+        newEnemy.x++;
     }
-  }
+
+    if (newEnemy.x >= enemyContainer.width) {
+        newEnemy.x = enemyContainer.width;
+        newEnemy.direction = 'left';
+    } else if (newEnemy.x <= 0) {
+        newEnemy.x = 0;
+        newEnemy.direction = 'right';
+    }
+    enemyElement.style.left = `${newEnemy.x}px`;
+}
