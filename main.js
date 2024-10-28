c030accfc196a88593a7d120f368e23c41b24db5
eediallo <elhhabdouldiallo@gmail.com>
2024-10-28 17:08:47 +0000

    clear main.js up by moving code the config file

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
-import {Enemy } from "./js/enemyClass.js";
 import { draw } from "./js/draw.js";
-import { EnemyContainer } from "./js/enemyContainer.js";
+import { updateEnemyPosition } from "./js/updateEnemyPosition.js";
+import { enemyElement } from "./js/config.js";
+import { newEnemy } from "./js/config.js";
 
-const enemyContainer = new EnemyContainer(1000, 400, "white", 2, "solid");
-const enemyContainerElement = document.querySelector(".enemy-container");
-enemyContainer.applyStyles(enemyContainerElement);
-const enemyElement = document.createElement("div");
-
-const newEnemy = new Enemy(50, 50, 15, 15);
 draw(enemyElement, newEnemy);
-
-
-//===============update======================
-function update() {
-  newEnemy.x++;
-  enemyElement.style.left = `${newEnemy.x}px`;
-  if (newEnemy.x >= enemyContainer.width) {
-    newEnemy.x = 0;
-  }
-}
-
-//==========Set interval==========
-setInterval(update, 10);
-
-//call update
-update();
+updateEnemyPosition();
+setInterval(updateEnemyPosition, 10);
