868d7b4a17715ef80b3315a1f820cda1a3326d09
eediallo <elhhabdouldiallo@gmail.com>
2024-10-28 15:50:25 +0000

    creates update  function and setInterval

--- js/enemyClass.js
+++ js/enemyClass.js
@@ -7,17 +7,30 @@ class enemy {
   }
 }
 
-const newEnemy = new enemy(50, 50);
+const newEnemy = new enemy(50, 50, 15, 15);
 
 function draw(container, enemyInstance) {
-    const enemyContainer = document.querySelector('.enemy-container')
-    container.classList.add("enemy");
-    container.style.width = `${enemyInstance.width}px`;
-    container.style.height = `${enemyInstance.height}px`;
+  const enemyContainer = document.querySelector(".enemy-container");
+  container.classList.add("enemy");
+  container.style.width = `${enemyInstance.width}px`;
+  container.style.height = `${enemyInstance.height}px`;
+  container.style.position = "absolute";
+  container.style.left = `${enemyInstance.x}px`;
+  container.style.top = `${enemyInstance.y}px`;
 
-    enemyContainer.append(container);
+  enemyContainer.append(container);
 }
 
-draw(document.createElement("div"), newEnemy);
+const enemyElement = document.createElement("div");
+draw(enemyElement, newEnemy);
+
+function update() {
+  newEnemy.x++;
+  enemyElement.style.left = `${newEnemy.x}px`;
+}
+
+setInterval(update, 10);
+
+update();
 
 console.log(newEnemy);
