bbed920241f932f02d8c7d64eb54f0b4a24ee710
eediallo <elhhabdouldiallo@gmail.com>
2024-10-28 15:23:07 +0000

    creates the container of enemy class

--- js/enemyClass.js
+++ js/enemyClass.js
@@ -1,21 +1,23 @@
-
-
 class enemy {
-    constructor(width, height, x = 0, y = 0){
-        this.width = width
-        this.height = height
-        this.x = x
-        this.y = y
-    }
+  constructor(width, height, x = 0, y = 0) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+  }
 }
 
+const newEnemy = new enemy(23, 59);
 
-const newEnemy = new enemy(23, 59)
+function draw(container, enemyInstance) {
+    const bodyElement = document.body
+    container.classList.add("enemy-container");
+    container.style.width = `${enemyInstance.width}px`;
+    container.style.height = `${enemyInstance.height}px`;
 
-
-function draw(){
-    
+    bodyElement.append(container);
 }
 
+draw(document.createElement("div"), newEnemy);
 
-console.log(newEnemy)
+console.log(newEnemy);
