7574c306c10fc27942155b0d876abf3dd629e5dd
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 11:01:47 +0000

    updates position of enemies.

--- js/updateEnemyPosition.js
+++ js/updateEnemyPosition.js
@@ -1,22 +1,26 @@
-import { enemyElement } from "./config.js";
-import { newEnemy } from "./config.js";
+import { enemies } from "./config.js";
 import { enemyContainer } from "./config.js";
 
-//===============update======================
-export function updateEnemyPosition() {
-  if (newEnemy.direction === "left") {
-    newEnemy.x--;
-    console.log(newEnemy.x)
-  } else {
-    newEnemy.x++;
-  }
+export function updateEnemyPositions() {
+  enemies.forEach((enemy, index) => {
+    if (enemy.direction === "left") {
+      enemy.x--;
+    } else {
+      enemy.x++;
+    }
 
-  if (newEnemy.x >= enemyContainer.width) {
-    newEnemy.x = enemyContainer.width;
-    newEnemy.direction = "left";
-  } else if (newEnemy.x <= 0) {
-    newEnemy.x = 0;
-    newEnemy.direction = "right";
-  }
-  enemyElement.style.left = `${newEnemy.x}px`;
+    // Reverse direction if an enemy hits container bounds
+    if (enemy.x >= enemyContainer.width - enemy.width) {
+      enemy.x = enemyContainer.width - enemy.width;
+      enemy.direction = "left";
+    } else if (enemy.x <= 0) {
+      enemy.x = 0;
+      enemy.direction = "right";
+    }
+
+    // Update the enemy element’s position in the DOM
+    const enemyElement = document.querySelector(`.enemy-${index}`);
+    enemyElement.style.left = `${enemy.x}px`;
+  });
 }
+
