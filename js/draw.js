8e3cb111df607fba362f0447367ccbadef6ee059
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 16:02:22 +0000

    sets horizontal and vertical position of the enemy

--- js/draw.js
+++ js/draw.js
@@ -5,10 +5,11 @@ function draw(container, enemyInstance, index) {
   container.style.width = `${enemyInstance.width}px`;
   container.style.height = `${enemyInstance.height}px`;
   container.style.position = "absolute";
-  container.style.left = `${enemyInstance.x}px`;
-  container.style.top = `${enemyInstance.y}px`;
-  container.style.backgroundColor = enemyInstance.color; // Optional color styling
+  container.style.left = `${enemyInstance.x}px`; // Set horizontal position
+  container.style.top = `${enemyInstance.y}px`; // Set vertical position
+  container.style.backgroundColor = enemyInstance.color;
 
+  // Append the enemy to the enemy container
   enemyContainerElement.append(container);
 }
 
