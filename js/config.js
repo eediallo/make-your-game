a6b0b7c77cb6d8a8012e60e17200cae7f2fd63a8
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 15:58:15 +0000

    updates width and height of enemies container

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ import { EnemyContainer } from "./enemyContainer.js";
 import { Enemy } from "./enemyClass.js";
 import { draw } from "./draw.js";
 
-const enemyContainer = new EnemyContainer(1000, 400, "white", 2, "solid");
+const enemyContainer = new EnemyContainer(1300, 600, "white", 2, "solid");
 const enemyContainerElement = document.querySelector(".enemy-container");
 enemyContainer.applyStyles(enemyContainerElement);
 
