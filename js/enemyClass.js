89a379977f5159fc50eac22403f1d349b7af3bbf
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 10:57:34 +0000

    adds direction to enemy class

--- js/enemyClass.js
+++ js/enemyClass.js
@@ -1,9 +1,18 @@
 class Enemy {
-  constructor(width, height, x = 0, y = 0, color, line) {
+  constructor(
+    width,
+    height,
+    x = 0,
+    y = 0,
+    color = "black",
+    direction = "right"
+  ) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
+    this.color = color;
+    this.direction = direction; // track movement direction
   }
 }
 
