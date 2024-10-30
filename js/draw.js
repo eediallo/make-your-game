af28cac8a3e6ed002e3030f1fffb7098f63d1c6b
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 11:00:47 +0000

    adds index to create a class for each enemy

--- js/draw.js
+++ js/draw.js
@@ -1,16 +1,15 @@
-const enemyContainerElement = document.querySelector(".enemy-container");
+import { enemyContainerElement } from "./config.js";
 
-function draw(container, enemyInstance) {
-    container.classList.add("enemy");
-    container.style.width = `${enemyInstance.width}px`;
-    container.style.height = `${enemyInstance.height}px`;
-    container.style.position = "absolute";
-    container.style.left = `${enemyInstance.x}px`;
-    container.style.top = `${enemyInstance.y}px`;
-  
-    enemyContainerElement.append(container);
-  }
-  
-  export {
-    draw
-  }
+function draw(container, enemyInstance, index) {
+  container.classList.add(`enemy`, `enemy-${index}`);
+  container.style.width = `${enemyInstance.width}px`;
+  container.style.height = `${enemyInstance.height}px`;
+  container.style.position = "absolute";
+  container.style.left = `${enemyInstance.x}px`;
+  container.style.top = `${enemyInstance.y}px`;
+  container.style.backgroundColor = enemyInstance.color; // Optional color styling
+
+  enemyContainerElement.append(container);
+}
+
+export { draw };
