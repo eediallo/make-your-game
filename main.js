e1675d08c453f99f37d9ac25fd8df40fb7412cb7
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 15:57:32 +0000

    replaces setInterval with the animate function

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
 import { createEnemies } from "./js/createEnemies.js";
 //Create 11 enemies and draw them initially
 createEnemies();
-
-// Continuously update the positions of all enemies
-// setInterval(updateEnemyPositions, 10);
-
 function animate() {
   updateEnemyPositions();
   requestAnimationFrame(animate);
