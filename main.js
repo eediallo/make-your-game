3ea59611e3a2ad16a79d9f6b711d0c9407e28b58
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 17:26:24 +0000

    calls drawPlayer function to draw the player

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
 import { createEnemies } from "./js/createEnemies.js";
+import { drawPlayer } from "./js/drawPlayer.js";
 //Create 11 enemies and draw them initially
 createEnemies();
+drawPlayer();
 function animate() {
   updateEnemyPositions();
   requestAnimationFrame(animate);
