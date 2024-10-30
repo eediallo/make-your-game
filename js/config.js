3ab394cb1b0eccacaea7619f8365e6e0f3996233
eediallo <elhhabdouldiallo@gmail.com>
2024-10-30 17:36:22 +0000

    updates x position of player

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@ import { Player } from "./playerClass.js";
 const enemyContainer = new EnemyContainer(1300, 600, "white", 2, "solid");
 const enemyContainerElement = document.querySelector(".enemy-container");
 enemyContainer.applyStyles(enemyContainerElement);
-const newPlayer = new Player(100, 100, 0, 0);
+const newPlayer = new Player(100, 100, 50, 0);
 
 export { enemyContainer, enemyContainerElement, newPlayer };
