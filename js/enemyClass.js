899a5868a83bc756f16f99617224ec38eb9801ee
daisyelem <daisyelem714@gmail.com>
2024-11-04 16:52:59 +0000

    5 rows animated enemies

--- js/enemyClass.js
+++ js/enemyClass.js
@@ -1,46 +1,57 @@
-export class Enemy{
-    constructor(width, height, x=0, y=0, row) {
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        this.speed = 5;
-        this.leftEdge = false;
-        this.rightEdge = false;
-        this.direction = ''
-        this.image = './assets/SpriteSheet.png'
+class Enemy {
+    constructor(game, column, row) {
+        this.game = game;
+        this.markedForDeletion = false;
+        this.width = 64;
+        this.height = 64;
+        this.column = column;
         this.row = row;
+        this.x = this.column * (this.width + 10);
+        this.y = this.row * (this.height + 10) + this.game.row;
+        this.frame = 0;
+        this.frameTimer = 0;
+        this.frameInterval = 200;
     }
-    draw (container) {
-        const enemy = document.createElement('div')
-        enemy.className = 'enemy'
+    update(deltaTime) {
+        this.x += this.game.speed
+        this.y = this.row * (this.height + 10) + this.game.row
+        if (this.frameTimer > this.frameInterval){
+            this.frame = this.frame==0? 1:0;
+            this.frameTimer = 0
+        } else this.frameTimer += deltaTime
+        
+
+    }
+    draw(container) {
+        const enemy = document.createElement("div");
+        enemy.className = "enemy1";
         enemy.style.width = `${this.width}px`;
         enemy.style.height = `${this.height}px`;
         enemy.style.left = `${this.x}px`;
         enemy.style.top = `${this.y}px`;
         enemy.style.background = `url(${this.image})`;
-        // enemy.style.
-        container.appendChild(enemy)
-    }
-    update(gameSpeed) {
-        if (this.direction == 'left') {
-            gameSpeed *= -1
-        }
-        this.x += gameSpeed
-        if (this.x + this.width >= 1000){
-            this.direction = 'left'
-            this.y += 5;
-            
-        } else if (this.x <= 0){
-            this.direction = 'right'
-            this.y += 5
-            gameSpeed *= -1
-            this.x += 2*gameSpeed
-        }
-        
-        
-        
-        
+        enemy.style.backgroundPositionX = `${this.frame * 64}px`
+        container.appendChild(enemy);
     }
 }
 
+export class Enemy1 extends Enemy {
+    constructor(game, column, row) {
+        super(game, column, row);
+        this.image = "./assets/SpriteSheet1.png";
+    }
+}
+
+export class Enemy2 extends Enemy {
+    constructor(game, column, row) {
+        super(game, column, row);
+        this.image = "./assets/SpriteSheet2.png";
+    }
+}
+
+export class Enemy3 extends Enemy {
+    constructor(game, column, row) {
+        super(game, column, row);
+        this.image = "./assets/SpriteSheet3.png";
+    }
+}
--- main.js
+++ main.js
@@ -1,28 +1,76 @@
-import { Enemy } from "./js/enemyClass.js";
-let enemies = [];
-let enemiesPerRow = 11;
-let gameSpeed = 1;
-let rowHeight = 0
-let enemySize = 64
-for (let i = 0; i < enemiesPerRow; i++){
-    enemies.push(new Enemy(enemySize, enemySize, i*(enemySize +10), 1));
-    enemies.push(new Enemy(enemySize, enemySize, i*(enemySize +10), enemySize + 10, 2));
-    enemies.push(new Enemy(enemySize, enemySize, i*(enemySize +10), 2 *(enemySize + 10), 3));
-    enemies.push(new Enemy(enemySize, enemySize, i*(enemySize +10), 3 *(enemySize + 10), 4));
-    enemies.push(new Enemy(enemySize, enemySize, i*(enemySize +10), 4 *(enemySize + 10), 5));
-}
-const container = document.getElementById('container')
-let direction = 'right'
-setInterval(function(){
-    container.innerHTML = ''
-    enemies.forEach(enemy => {
-        enemy.direction = direction
-        enemy.y = rowHeight + (enemy.row * enemySize)
-        enemy.draw(container)
-        
-        enemy.update(gameSpeed)
-        direction = enemy.direction
-        // rowHeight = enemy.y 
-    })
-},10)
+import { Enemy1, Enemy2, Enemy3 } from "./js/enemyClass.js";
+window.addEventListener("load", function () {
+    let gameHeight = 600;
+    let gameWidth = 1000;
+    const container = document.getElementById("container");
+    container.style.height = `${gameHeight}px`;
+    container.style.width = `${gameWidth}px`;
+
+    class Game {
+        constructor(width, height) {
+            (this.width = width),
+                (this.height = height),
+                (this.speed = 5),
+                (this.row = 0),
+                (this.maxRow = 4),
+                (this.enemies = []);
+        }
+        update(deltaTime) {
+            this.enemies.forEach((enemy, i) => {
+                enemy.update(deltaTime);
+                if (
+                    enemy.x >= this.width - enemy.width &&
+                    enemy.row == this.maxRow
+                ) {
+                    this.speed *= -1;
+
+                    this.row += 5;
+                }
+                // if (enemy.column == 10) enemy.x -= 2*this.speed
+                console.log(
+                    i,
+                    enemy.game.speed,
+                    enemy.x,
+                    enemy.x + enemy.width
+                );
+                if (enemy.x <= 0) {
+                    enemy.x -= 2 * this.speed;
+                    this.speed *= -1;
+                    this.row += 5;
+                }
+            });
+        }
+        draw(context) {
+            this.enemies.forEach((enemy) => {
+                enemy.draw(context);
+            });
+        }
+        makeEnemies() {
+            for (let i = 0; i < 11; i++) {
+                this.enemies.push(new Enemy1(this, i, 0));
+                this.enemies.push(new Enemy2(this, i, 1));
+                this.enemies.push(new Enemy2(this, i, 2));
+                this.enemies.push(new Enemy3(this, i, 3));
+                this.enemies.push(new Enemy3(this, i, 4));
+            }
+        }
+    }
+
+    const game = new Game(gameWidth, gameHeight);
+    let lastTime = 0;
+    game.makeEnemies();
+    let count = 0;
+    function animate(timeStamp) {
+        container.innerHTML = "";
+        count++;
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        game.draw(container);
+        game.update(deltaTime);
+        // if (count < 100) {
+        requestAnimationFrame(animate);
+        // }
+    }
     
+    animate(0);
+});
