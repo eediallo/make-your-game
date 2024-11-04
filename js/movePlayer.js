130e0fad716791b7533f24e30d05dab9805cadaf
eediallo <elhhabdouldiallo@gmail.com>
2024-11-04 22:28:28 +0000

    prevent the player element from moving outside the edges of its container

--- js/movePlayer.js
+++ js/movePlayer.js
@@ -4,16 +4,25 @@ import { drawPlayer } from "./drawPlayer.js";
 drawPlayer(); // allow access to player element
 
 const player = document.querySelector(".player");
+const playerContainer = document.querySelector(".enemy-container");
 let posX = newPlayer.x;
 let posY = newPlayer.y;
 const step = 10;
 
 function movePlayer(event) {
-  if (event.key === "ArrowLeft") {
+  const playerRect = player.getBoundingClientRect();
+  const playerContainerRect = playerContainer.getBoundingClientRect();
+  if (
+    event.key === "ArrowLeft" &&
+    playerRect.left - step >= playerContainerRect.left
+  ) {
     posX -= step;
   }
 
-  if (event.key === "ArrowRight") {
+  if (
+    event.key === "ArrowRight" &&
+    playerRect.right + step <= playerContainerRect.right
+  ) {
     posX += step;
   }
 
