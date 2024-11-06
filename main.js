c787ee756883daf453639c68562e7211b1d0de61
eediallo <elhhabdouldiallo@gmail.com>
2024-11-06 13:11:56 +0000

    call creates bullet to main to create the bullet

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ import { drawPlayer } from "./js/drawPlayer.js";
 import { movePlayer } from "./js/movePlayer.js";
 import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
 import { createEnemies } from "./js/createEnemies.js";
+import { createBullet } from "./js/createBullet.js";
 
 createEnemies();
 drawPlayer();
 
 // Event listener to move the player
 document.addEventListener("keydown", movePlayer);
+
+createBullet();
 
 function animate() {
   updateEnemyPositions();
