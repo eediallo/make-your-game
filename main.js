47b7af839e0322438f7b61019dd88adcfb07933b
eediallo <elhhabdouldiallo@gmail.com>
2024-11-06 17:52:20 +0000

    adds event listener for shooting bullet

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { movePlayer } from "./js/movePlayer.js";
 import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
 import { createEnemies } from "./js/createEnemies.js";
 import { createBullet } from "./js/createBullet.js";
+import { shootBullet } from "./js/shootBullet.js";
 
 createEnemies();
 drawPlayer();
@@ -10,7 +11,8 @@ createBullet();
 
 // Event listener to move the player
 document.addEventListener("keydown", movePlayer);
-
+// Event listener to shoot the bullet when space is pressed
+document.addEventListener("keydown", shootBullet);
 function animate() {
   updateEnemyPositions();
   requestAnimationFrame(animate);
