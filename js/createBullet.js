7bf5dc072e2aa8196c0bf96cb3b89583abbb71d1
eediallo <elhhabdouldiallo@gmail.com>
2024-11-06 15:36:53 +0000

    uses offset property to to position bullet at top player

--- js/createBullet.js
+++ js/createBullet.js
@@ -1,3 +1,4 @@
+import { newPlayer } from "./config.js";
 class Bullet {
   constructor(width, height, bulletSpeed, x, y) {
     this.width = width;
@@ -9,15 +10,21 @@ class Bullet {
   }
 }
 
-const newBullet = new Bullet(20, 20, 20, 0, 0);
+const newBullet = new Bullet(10, 10, 20, 0, 0);
 
-function createBullet(playerElement) {
+function createBullet() {
+  const player = document.querySelector(".player");
+  const playerLeft =
+    player.offsetLeft + player.offsetWidth / 2 - newBullet.width / 2;
+  const playerTop = player.offsetTop;
   const bullet = document.createElement("div");
   bullet.classList.add("bullet");
   bullet.style.width = `${newBullet.width}px`;
   bullet.style.height = `${newBullet.height}px`;
   bullet.style.position = "absolute";
   bullet.style.backgroundColor = "red";
+  bullet.style.left = `${playerLeft}px`;
+  bullet.style.top = `${playerTop}px`;
 
   document.querySelector(".enemy-container").appendChild(bullet);
 }
