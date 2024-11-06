0ea556a15bac5134bc768c7589cf58a130e9eca4
eediallo <elhhabdouldiallo@gmail.com>
2024-11-06 13:09:46 +0000

    restructure main.js by adding event listener to move player

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+import { drawPlayer } from "./js/drawPlayer.js";
+import { movePlayer } from "./js/movePlayer.js";
 import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
 import { createEnemies } from "./js/createEnemies.js";
 
-createEnemies(); //Create 11 enemies and draw them initially
+createEnemies();
+drawPlayer();
+
+// Event listener to move the player
+document.addEventListener("keydown", movePlayer);
+
 function animate() {
   updateEnemyPositions();
   requestAnimationFrame(animate);
