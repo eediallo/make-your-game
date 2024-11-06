8f65ffe2004ca2b000252f6aed59a1a83fddf273
eediallo <elhhabdouldiallo@gmail.com>
2024-11-06 13:11:18 +0000

    creates bullet

--- js/createBullet.js
+++ js/createBullet.js
@@ -1,18 +1,17 @@
 class Bullet {
-  constructor(width, height, bulletSpeed) {
+  constructor(width, height, bulletSpeed, x, y) {
     this.width = width;
     this.height = height;
     this.bulletSpeed = bulletSpeed;
     this.activeBullet = null; // Track if there's a current active bullet
+    this.x = x;
+    this.y = y;
   }
 }
 
-const newBullet = new Bullet(5, 10, 20);
+const newBullet = new Bullet(20, 20, 20, 0, 0);
 
-function shootBullet(playerElement) {
-  // Check if there’s already an active bullet
-  if (newBullet.activeBullet) return;
-
+function createBullet(playerElement) {
   const bullet = document.createElement("div");
   bullet.classList.add("bullet");
   bullet.style.width = `${newBullet.width}px`;
@@ -20,46 +19,7 @@ function shootBullet(playerElement) {
   bullet.style.position = "absolute";
   bullet.style.backgroundColor = "red";
 
-  // Initially position the bullet relative to the player
-  positionBullet(bullet, playerElement);
-
   document.querySelector(".enemy-container").appendChild(bullet);
-  newBullet.activeBullet = bullet; // Track the bullet as "attached"
-  moveBulletUpwards(bullet);
 }
 
-// Move the bullet upwards until it reaches the top of the container
-function moveBulletUpwards(bullet) {
-  const containerRect = document
-    .querySelector(".enemy-container")
-    .getBoundingClientRect();
-
-  const bulletInterval = setInterval(() => {
-    const bulletRect = bullet.getBoundingClientRect();
-
-    // Stop bullet if it reaches the top of the container
-    if (bulletRect.top <= containerRect.top) {
-      bullet.remove(); // Remove the bullet when it goes out of bounds
-      clearInterval(bulletInterval);
-      newBullet.activeBullet = null; // Allow new bullets to be created
-    } else {
-      // Move the bullet up by bulletSpeed pixels
-      bullet.style.top = `${
-        parseFloat(bullet.style.top) - newBullet.bulletSpeed
-      }px`;
-    }
-  }, 20);
-}
-
-// Position the bullet at the center top of the player
-function positionBullet(bullet, playerElement) {
-  const playerLeft =
-    playerElement.offsetLeft +
-    playerElement.offsetWidth / 2 -
-    newBullet.width / 2;
-  const playerTop = playerElement.offsetTop;
-  bullet.style.left = `${playerLeft}px`;
-  bullet.style.top = `${playerTop}px`;
-}
-
-export { shootBullet, newBullet };
+export { createBullet, newBullet };
