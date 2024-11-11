7e09c2a0533279225eb47b0da8529aee85f1ee71
eediallo <elhhabdouldiallo@gmail.com>
2024-11-11 16:31:22 +0000

    removes import of create bullet

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { drawPlayer } from "./js/drawPlayer.js";
 import { movePlayer } from "./js/movePlayer.js";
 import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
 import { createEnemies } from "./js/createEnemies.js";
-import { createBullet } from "./js/createBullet.js";
 import { shootBullet } from "./js/shootBullet.js";
 
 createEnemies();
