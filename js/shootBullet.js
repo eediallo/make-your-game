cfeb7b8f79fcc53161e4d93b8493b7e72941f07c
eediallo <elhhabdouldiallo@gmail.com>
2024-11-11 16:52:31 +0000

    check if there is no active bullet

--- js/shootBullet.js
+++ js/shootBullet.js
@@ -1,10 +1,10 @@
-import { createBullet } from "./createBullet.js";
+import { createBullet, newBullet } from "./createBullet.js";
 import { moveBulletUpwards } from "./moveBulletUpward.js";
 import { updateBulletPosition } from "./updateBulletPosition.js";
 
 function shootBullet() {
   document.addEventListener("keydown", (event) => {
-    if (event.key === " ") {
+    if (event.key === " " && !newBullet.activeBullet) {
       const player = document.querySelector(".player");
       createBullet(player); // Create bullet if it doesn't exist
       const bullet = document.querySelector(".bullet");
