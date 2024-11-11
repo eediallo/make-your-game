91d80fe60e2d7ab9f46f48675d3b6fe8b1b94a4c
eediallo <elhhabdouldiallo@gmail.com>
2024-11-11 12:29:13 +0000

    moves function

--- js/shootBullet.js
+++ js/shootBullet.js
@@ -1,28 +1,5 @@
-import { newBullet } from "./createBullet.js";
 import { createBullet } from "./createBullet.js";
-
-// Move the bullet upwards until it reaches the top of the container
-function moveBulletUpwards(bullet) {
-  const containerRect = document
-    .querySelector(".enemy-container")
-    .getBoundingClientRect();
-
-  const bulletInterval = setInterval(() => {
-    const bulletRect = bullet.getBoundingClientRect();
-
-    // Stop bullet if it reaches the top of the container
-    if (bulletRect.top <= containerRect.top) {
-      bullet.remove();
-      clearInterval(bulletInterval);
-      newBullet.activeBullet = null; // Allow new bullets to be created
-    } else {
-      // Move the bullet up by bulletSpeed pixels
-      bullet.style.top = `${
-        parseFloat(bullet.style.top) - newBullet.bulletSpeed
-      }px`;
-    }
-  }, 20);
-}
+import { moveBulletUpwards } from "./moveBulletUpward.js";
 
 function shootBullet() {
   document.addEventListener("keydown", (event) => {
