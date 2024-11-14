c295013c2fb8e6ba7ed9fe432b9da540a4ba2cbf
eediallo <elhhabdouldiallo@gmail.com>
2024-11-14 14:04:35 +0000

    updates main

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-import { drawPlayer } from "./js/drawPlayer.js";
-import { movePlayer } from "./js/movePlayer.js";
-import { updateEnemyPositions } from "./js/updateEnemyPosition.js";
-import { createEnemies } from "./js/createEnemies.js";
-import { shootBullet } from "./js/shootBullet.js";
-
-createEnemies();
-drawPlayer();
-document.addEventListener("keydown", movePlayer); // Event listener to move the player
-shootBullet();
-
-function animate() {
-  updateEnemyPositions();
-  requestAnimationFrame(animate);
-}
-
-animate();
+import { startGame } from "./js/startGame.js";
+const startGameBtn = document.querySelector("#start-game-btn");
+startGameBtn.addEventListener("click", (btn) => {
+  startGame(startGameBtn);
+});
