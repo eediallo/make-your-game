cefea2bb287c0ca930be9ac327a86ae3808ff10a
eediallo <elhhabdouldiallo@gmail.com>
2024-11-14 15:07:47 +0000

    import gameTimeCouter func

--- js/startGame.js
+++ js/startGame.js
@@ -3,6 +3,7 @@ import { movePlayer } from "./movePlayer.js";
 import { updateEnemyPositions } from "./updateEnemyPosition.js";
 import { createEnemies } from "./createEnemies.js";
 import { shootBullet } from "./shootBullet.js";
+import { gameTimeCounter } from "./updateGameTime.js";
 
 function startGame(startGameBtn) {
   createEnemies();
@@ -15,6 +16,8 @@ function startGame(startGameBtn) {
     requestAnimationFrame(animate);
   }
   animate();
+
+  gameTimeCounter(); // start the counter in seconds
   startGameBtn.disabled = true;
 }
 
