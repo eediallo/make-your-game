af71c192acce2de06b63731070584b28072a6b7c
eediallo <elhhabdouldiallo@gmail.com>
2024-11-14 18:06:40 +0000

    removes border around game container

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 import { EnemyContainer } from "./enemyContainer.js";
 import { Player } from "./playerClass.js";
 
-const enemyContainer = new EnemyContainer(1200, 600, "white", 2, "solid");
+const enemyContainer = new EnemyContainer(1200, 600, "white", 2);
 const enemyContainerElement = document.querySelector(".enemy-container");
 enemyContainer.applyStyles(enemyContainerElement);
 const newPlayer = new Player(100, 100, 50, 0);
