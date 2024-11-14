1a4334e274bc9601d068fba742a16c2af0a64d1b
eediallo <elhhabdouldiallo@gmail.com>
2024-11-14 13:13:15 +0000

    creates a function to check for enemy bullet collision

--- js/playerHitEnemy.js
+++ js/playerHitEnemy.js
@@ -1,22 +1,31 @@
-import { newBullet } from "./createBullet.js";
 import { enemies } from "./createEnemies.js";
 
-function playerEnemyCollision(bullet) {
+function isEnemyBulletCollision(bullet) {
+  const bulletRect = bullet.getBoundingClientRect(); // Get the bullet's current position and size
+
   for (let enemy of enemies) {
-    console.log(enemy);
+    const enemyElement = enemy.element;
+    const enemyRect = enemyElement.getBoundingClientRect(); // Get the enemy's current position and size
+
+    // Check if the bullet and the enemy have collided
     if (
-      bullet.x + bullet.width >= enemy.x &&
-      bullet.x <= enemy.x + enemy.width &&
-      bullet.y + bullet.height >= enemy.y &&
-      bullet.y <= enemy.y + enemy.height
+      bulletRect.left + bulletRect.width >= enemyRect.left &&
+      bulletRect.left <= enemyRect.left + enemyRect.width &&
+      bulletRect.top + bulletRect.height >= enemyRect.top &&
+      bulletRect.top <= enemyRect.top + enemyRect.height
     ) {
-      return true;
+     
+      bullet.remove();
+      enemyElement.remove(); // Remove enemy DOM element
+
+      // Remove enemy from the enemies array
+      const index = enemies.indexOf(enemy);
+      if (index !== -1) {
+        enemies.splice(index, 1); // Remove enemy from array
+      }
+      break; // Stop checking for further collisions once one is detected
     }
   }
-
-  return false;
 }
 
-console.log(playerEnemyCollision(newBullet));
-
-export { playerEnemyCollision };
+export { isEnemyBulletCollision };
