67cff172e16fd7a65b23dda9add4f8d92f09add8
eediallo <elhhabdouldiallo@gmail.com>
2024-11-14 15:07:07 +0000

    removes unused param

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { startGame } from "./js/startGame.js";
 const startGameBtn = document.querySelector("#start-game-btn");
-startGameBtn.addEventListener("click", (btn) => {
+startGameBtn.addEventListener("click", () => {
   startGame(startGameBtn);
 });
