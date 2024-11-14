7adf5cfff60264d163ef15dd30acb06814aa5527
eediallo <elhhabdouldiallo@gmail.com>
2024-11-14 18:06:23 +0000

    adds start sound

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+import { playStart } from "./js/playSounds.js";
 import { startGame } from "./js/startGame.js";
+
+document.addEventListener("DOMContentLoaded", playStart);
 const startGameBtn = document.querySelector("#start-game-btn");
 startGameBtn.addEventListener("click", () => {
   startGame(startGameBtn);
