d6a14915db7c77c2b5daef243aff11f8c38f1eda
eediallo <elhhabdouldiallo@gmail.com>
2024-11-15 12:05:38 +0000

    updates startGame logic

--- js/startGame.js
+++ js/startGame.js
@@ -1,9 +1,9 @@
-import { drawPlayer } from "./drawPlayer.js";
+import { drawPlayer, hidePlayer } from "./drawPlayer.js";
 import { movePlayer } from "./movePlayer.js";
-import { updateEnemyPositions } from "./updateEnemyPosition.js";
+import { updateEnemyPositions, hideEnemies } from "./updateEnemyPosition.js";
 import { createEnemies } from "./createEnemies.js";
 import { shootBullet } from "./shootBullet.js";
-import { gameTimeCounter } from "./updateGameTime.js";
+import { gameTimeCounter, counter, maxTime } from "./updateGameTime.js";
 
 function startGame(startGameBtn) {
   createEnemies();
@@ -13,12 +13,25 @@ function startGame(startGameBtn) {
 
   function animate() {
     updateEnemyPositions();
-    requestAnimationFrame(animate);
+    if (counter < maxTime) {
+      requestAnimationFrame(animate);
+    }
   }
   animate();
 
-  gameTimeCounter(); // start the counter in seconds
+  // start the game timer
+  let intervalId = setInterval(gameTimeCounter, 1000);
+
+  // start the counter in seconds
   startGameBtn.disabled = true;
+
+  setTimeout(() => {
+    clearInterval(intervalId); // Clear the interval locally
+    hidePlayer();
+    hideEnemies();
+    const gameContainer = document.getElementById("gameContainer");
+    gameContainer.innerHTML = "<p>Game Over!</p>";
+  }, maxTime * 1000);
 }
 
 export { startGame };
