f7711919142c8897c73535e63ba621b60a5054b7
eediallo <elhhabdouldiallo@gmail.com>
2024-11-15 12:32:02 +0000

    removes auto play on page load

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import { playStart } from "./js/playSounds.js";
 import { startGame } from "./js/startGame.js";
 
-document.addEventListener("DOMContentLoaded", playStart);
 const startGameBtn = document.querySelector("#start-game-btn");
 startGameBtn.addEventListener("click", () => {
   startGame(startGameBtn);
