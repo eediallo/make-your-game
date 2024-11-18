b3903dcb75f7b36a0f8ec58d493908e09149f735
eediallo <elhhabdouldiallo@gmail.com>
2024-11-18 14:13:21 +0000

    removes toggle pause in main

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 import { startGame } from "./js/startGame.js";
-import {  togglePause } from "./js/togglePause.js";
+import { togglePause } from "./js/togglePause.js";
 
 const startGameBtn = document.querySelector("#start-game-btn");
 startGameBtn.addEventListener("click", () => {
   startGame(startGameBtn);
 });
-
-const timerController = {
-  intervalId: null,
-  isPaused: false,
-};
-
-const pauseBtn = document.querySelector(".pause-btn");
-togglePause(pauseBtn, timerController);
