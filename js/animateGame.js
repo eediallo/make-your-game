dd8618b3ef6c2f1876697e80333dafbfe9c3177c
eediallo <elhhabdouldiallo@gmail.com>
2024-11-18 11:34:14 +0000

    moves animate, stopAnimation and handleGameOver functions to their own file

---
+++ js/animateGame.js
@@ -0,0 +1,26 @@
+import { counter, maxTime } from "./updateGameTime.js";
+import { hidePlayer } from "./drawPlayer.js";
+import { hideEnemies } from "./hideEnemies.js";
+import { updateEnemyPositions } from "./updateEnemyPosition.js";
+let reqAnim;
+
+function animate() {
+  updateEnemyPositions();
+  if (counter < maxTime) {
+    reqAnim = requestAnimationFrame(animate);
+  }
+}
+
+function stopAnimation() {
+  cancelAnimationFrame(reqAnim);
+}
+
+function handleGameOver(intervalId) {
+  clearInterval(intervalId);
+  hidePlayer();
+  hideEnemies();
+  const gameContainer = document.querySelector(".game-container");
+  gameContainer.innerHTML = "<p>Game Over!</p>";
+}
+
+export { animate, stopAnimation, handleGameOver };
