ba7884b3190bf4638e019fefc6d49bea9b93e54a
eediallo <elhhabdouldiallo@gmail.com>
2024-11-18 13:31:35 +0000

    refactors toggle pause

--- js/togglePause.js
+++ js/togglePause.js
@@ -1,23 +1,27 @@
 import { movePlayer } from "./movePlayer.js";
 import { animate, stopAnimation } from "./animateGame.js";
 import { gameTimeCounter } from "./updateGameTime.js";
+import { shootBullet } from "./shootBullet.js";
 
 function togglePause(pauseGameBtn, timerController) {
-  pauseGameBtn.addEventListener("click", () => {
-    if (!timerController.isPaused) {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "p" && !timerController.isPaused) {
       stopAnimation();
       clearInterval(timerController.intervalId);
       document.removeEventListener("keydown", movePlayer);
+      document.removeEventListener("keydown", shootBullet);
       timerController.isPaused = true;
       pauseGameBtn.textContent = "Continue";
-    } else {
+    } else if (event.key === "c" && timerController.isPaused) {
       animate();
       timerController.intervalId = setInterval(gameTimeCounter, 1000);
       document.addEventListener("keydown", movePlayer);
+      document.addEventListener("keydown", shootBullet);
       timerController.isPaused = false;
       pauseGameBtn.textContent = "Pause";
     }
   });
 }
+
 
 export { togglePause };
