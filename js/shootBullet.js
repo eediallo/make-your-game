38a5ab2a09b74d57b02cf18ced01d0650d58724e
eediallo <elhhabdouldiallo@gmail.com>
2024-11-18 13:30:32 +0000

    refactors shoot bullet function

--- js/shootBullet.js
+++ js/shootBullet.js
@@ -3,16 +3,15 @@ import { moveBulletUpwards } from "./moveBulletUpward.js";
 import { playHitBulletSound } from "./playSounds.js";
 import { updateBulletPosition } from "./updateBulletPosition.js";
 
-function shootBullet() {
-  document.addEventListener("keydown", (event) => {
-    if (event.key === " " && !newBullet.activeBullet) {
-      const player = document.querySelector(".player");
-      createBullet(player); // Create bullet if it doesn't exist
-      const bullet = document.querySelector(".bullet");
-      updateBulletPosition(); // Ensure bullet position is updated
-      moveBulletUpwards(bullet);
-      playHitBulletSound();
-    }
-  });
+function shootBullet(event) {
+  if (event.key === " " && !newBullet.activeBullet) {
+    const player = document.querySelector(".player");
+    createBullet(player); // Create bullet if it doesn't exist
+    const bullet = document.querySelector(".bullet");
+    updateBulletPosition(); // Ensure bullet position is updated
+    moveBulletUpwards(bullet);
+    playHitBulletSound();
+  }
 }
+
 export { shootBullet };
