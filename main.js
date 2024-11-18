f68986ddedca97efe57aeee494e124ebd5e7eb45
eediallo <elhhabdouldiallo@gmail.com>
2024-11-18 11:33:23 +0000

    adds pause event

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 import { playStart } from "./js/playSounds.js";
 import { startGame } from "./js/startGame.js";
+import { togglePause } from "./js/togglePause.js";
 
 const startGameBtn = document.querySelector("#start-game-btn");
 startGameBtn.addEventListener("click", () => {
   startGame(startGameBtn);
 });
+
+const timerController = {
+  intervalId: null,
+  isPaused: false,
+};
+
+const pauseBtn = document.querySelector(".pause-btn");
+togglePause(pauseBtn, timerController);
