29a8e5d7716193d4f684dcc175ae606f1b4fafdd
eediallo <elhhabdouldiallo@gmail.com>
2024-11-18 11:34:42 +0000

    refactors  startGame function

--- js/startGame.js
+++ js/startGame.js
@@ -1,10 +1,9 @@
 import { drawPlayer, hidePlayer } from "./drawPlayer.js";
 import { movePlayer } from "./movePlayer.js";
-import { updateEnemyPositions } from "./updateEnemyPosition.js";
-import { hideEnemies } from "./hideEnemies.js";
 import { createEnemies } from "./createEnemies.js";
 import { shootBullet } from "./shootBullet.js";
-import { gameTimeCounter, counter, maxTime } from "./updateGameTime.js";
+import { gameTimeCounter, maxTime } from "./updateGameTime.js";
+import { animate } from "./animateGame.js";
 
 function startGame(startGameBtn) {
   createEnemies();
@@ -12,32 +11,12 @@ function startGame(startGameBtn) {
   document.addEventListener("keydown", movePlayer);
   shootBullet();
 
-  //==========animate====================
-  function animate() {
-    updateEnemyPositions();
-    if (counter < maxTime) {
-      requestAnimationFrame(animate);
-    }
-  }
   animate();
-
-  //=======================================
 
   let intervalId = setInterval(gameTimeCounter, 1000); // start the game timer
   startGameBtn.disabled = true;
 
-  //=======================handleGameOver========================
-  function handleGameOver(intervalId) {
-    clearInterval(intervalId);
-    hidePlayer();
-    hideEnemies();
-    const gameContainer = document.querySelector(".game-container");
-    gameContainer.innerHTML = "<p>Game Over!</p>";
-  }
-
   setTimeout(() => handleGameOver(intervalId), maxTime * 1000);
-
-  //===========================================================
 }
 
 export { startGame };
