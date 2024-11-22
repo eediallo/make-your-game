87ae4940e528f93cdec0f4ed9743326fe085fbf6
eediallo <elhhabdouldiallo@gmail.com>
2024-11-22 15:35:07 +0000

    deletes unused export

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import { startGame } from "./js/startGame.js";
-import { togglePause } from "./js/togglePause.js";
+import {startGame} from './js/startGame.js'
 
 const startGameBtn = document.querySelector("#start-game-btn");
 startGameBtn.addEventListener("click", () => {
