f06c0065724bfd722727a7fde7f4aaa6ef3b382f
eediallo <elhhabdouldiallo@gmail.com>
2024-11-22 17:12:16 +0000

    adds player lives

--- js/playerClass.js
+++ js/playerClass.js
@@ -2,5 +2,6 @@ export class Player {
   constructor(width, height, x, y, direction) {
     (this.width = width), (this.height = height), (this.x = x), (this.y = y);
     this.direction = "right";
+    this.lives = 10;
   }
 }
