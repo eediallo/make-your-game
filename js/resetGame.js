96f9dd3f1630a1a6c36375e2e8efe82b4361900e
eediallo <elhhabdouldiallo@gmail.com>
2024-11-22 17:13:40 +0000

    stop enemy shooting in reset

--- js/resetGame.js
+++ js/resetGame.js
@@ -1,30 +1,38 @@
 import { animate, stopAnimation } from "./animateGame.js";
-import { gameTimeCounter, counter, gameTimeIntervalId } from "./updateGameTime.js";
+import { gameTimeCounter, counter } from "./updateGameTime.js";
 import { movePlayer } from "./movePlayer.js";
 import { shootBullet } from "./shootBullet.js";
 import { createEnemies } from "./createEnemies.js";
 import { drawPlayer } from "./drawPlayer.js";
 import { newPlayer } from "./config.js";
 import { newBullet } from "./createBullet.js";
+import { stopEnemyShooting } from "./startEnemyShooting.js";
 
 function resetGame(timerController) {
   // Reset game state
+  counter = 1;
   newPlayer.x = 50;
   newPlayer.y = 0;
   newBullet.activeBullet = null;
   document.querySelector("#score").textContent = "Score: 0";
   document.querySelector("#game-time").textContent = "Time: 00";
+  document.querySelector("#lives").textContent = "Lives: 10";
+  newPlayer.lives = 10;
+
 
   const gameContainer = document.querySelector(".game-container");
   gameContainer.innerHTML = "";
 
+
   createEnemies();
   drawPlayer();
 
-  stopAnimation()
-  
-  // Restart the game
+  // Stop any ongoing animations and intervals
+  stopAnimation();
   clearInterval(timerController.intervalId);
+  stopEnemyShooting();
+
+  // Restart the game
   timerController.isPaused = false;
   timerController.intervalId = setInterval(gameTimeCounter, 1000);
   animate();
