88fd60dadb40d6493ae3d6744b5ac517ba6417a8
eediallo <elhhabdouldiallo@gmail.com>
2024-11-25 17:53:17 +0000

    displays stats of game when it is over

--- js/animateGame.js
+++ js/animateGame.js
@@ -3,6 +3,7 @@ import { updateEnemyPositions } from "./updateEnemyPosition.js";
 import { stopEnemyShooting } from "./startEnemyShooting.js";
 import { hideEnemies } from "./hideEnemies.js";
 import { hidePlayer } from "./drawPlayer.js";
+import { updateScore } from "./updateScore.js";
 let reqAnim;
 
 function animate() {
@@ -16,16 +17,42 @@ function stopAnimation() {
   cancelAnimationFrame(reqAnim);
 }
 
-
 function handleGameOver(intervalId) {
   clearInterval(intervalId);
   stopAnimation();
   stopEnemyShooting();
   hidePlayer();
   hideEnemies();
-  const gameContainer = document.querySelector(".game-container");
-  gameContainer.innerHTML = "<p>Game Over!</p>";
-}
 
+  const gameContainer = document.querySelector(".game-container");
+  const score = document.querySelector('#score')
+  const gameTimer = document.querySelector('#game-time')
+  const lives = document.querySelector('#lives')
+  gameContainer.innerHTML = `
+    <div class="scoreboard">
+      <h1>Game Over!</h1>
+      <p>${score.innerHTML}</p>
+       <p>${gameTimer.innerHTML}</p>
+      <p>${lives.innerHTML}</p>
+    </div>
+  `;
+
+  // Center the scoreboard
+  const scoreboardStyle = document.createElement("style");
+  scoreboardStyle.innerHTML = `
+    .scoreboard {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+      background-color: rgba(0, 0, 0, 0.8);
+      color: white;
+      padding: 20px;
+      border-radius: 10px;
+    }
+  `;
+  document.head.appendChild(scoreboardStyle);
+}
 
 export { animate, stopAnimation, handleGameOver };
