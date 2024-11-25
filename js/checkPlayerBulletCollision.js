b6d9b2af452b775e16d9ff495b59e4c78fe51db0
eediallo <elhhabdouldiallo@gmail.com>
2024-11-25 14:39:13 +0000

    check existence of player

--- js/checkPlayerBulletCollision.js
+++ js/checkPlayerBulletCollision.js
@@ -2,18 +2,21 @@ import { handlePlayerHit } from "./handlePlayerHit.js";
 
 function checkPlayerBulletCollision(bullet) {
   const player = document.querySelector(".player");
-  const playerRect = player.getBoundingClientRect();
-  const bulletRect = bullet.getBoundingClientRect();
-
-  if (
-    bulletRect.left < playerRect.right &&
-    bulletRect.right > playerRect.left &&
-    bulletRect.top < playerRect.bottom &&
-    bulletRect.bottom > playerRect.top
-  ) {
-    bullet.remove();
-    handlePlayerHit();
+  if(player){
+    const playerRect = player.getBoundingClientRect();
+    const bulletRect = bullet.getBoundingClientRect();
+    if (
+      bulletRect.left < playerRect.right &&
+      bulletRect.right > playerRect.left &&
+      bulletRect.top < playerRect.bottom &&
+      bulletRect.bottom > playerRect.top
+    ) {
+      bullet.remove();
+      handlePlayerHit();
+    }
   }
+
+ 
 }
 
 export { checkPlayerBulletCollision };
