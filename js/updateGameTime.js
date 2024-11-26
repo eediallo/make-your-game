bd3bb27939078082fbaa25d0d01cbf7497e7a376
eediallo <elhhabdouldiallo@gmail.com>
2024-11-26 00:20:02 +0000

    do not proceed when game is paused

--- js/updateGameTime.js
+++ js/updateGameTime.js
@@ -1,19 +1,28 @@
 import { formatUnit } from "./config.js";
+import { handleGameOver } from "./handleGameOver.js";
 
 const maxTime = 30;
 let counter = 1;
 let gameTimeIntervalId;
+let timerController;
 
 function gameTimeCounter() {
+  if (timerController.isPaused) return; // Do not update the counter if the game is paused
+
   const gameTimeElement = document.querySelector("#game-time");
   if (!gameTimeElement) return; // ensure element exist
   gameTimeElement.textContent = `Time: ${formatUnit(counter)}`;
-  counter++
+  counter++;
   if (counter >= maxTime) {
     clearInterval(gameTimeIntervalId);
+    handleGameOver(timerController); // Call handleGameOver when time is up
   }
 }
 
-const resetCounter = () => counter = 0
+const resetCounter = () => counter = 0;
 
-export { gameTimeCounter, maxTime, counter, gameTimeIntervalId, resetCounter };
+function setTimerController(controller) {
+  timerController = controller;
+}
+
+export { gameTimeCounter, maxTime, counter, gameTimeIntervalId, resetCounter, setTimerController };
