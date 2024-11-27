e1bd616ac461a032bc853dcbbfdf4f106782385a
eediallo <elhhabdouldiallo@gmail.com>
2024-11-27 12:31:06 +0000

    refactors start game

--- js/startGame.js
+++ js/startGame.js
@@ -1,19 +1,16 @@
 import { drawPlayer } from "./drawPlayer.js";
-import { movePlayer } from "./movePlayer.js";
 import { createEnemies } from "./createEnemies.js";
-import { shootBullet } from "./shootBullet.js";
 import { gameTimeCounter, setTimerController } from "./updateGameTime.js";
 import { animate } from "./animateGame.js";
 import { togglePause } from "./togglePause.js";
 import { startEnemyShooting } from "./startEnemyShooting.js";
+import { addMovePlayerAndShootBulletEvents } from "./addAndRemoveEvents.js";
 
 function startGame(startGameBtn) {
   createEnemies();
   drawPlayer();
   animate();
-  document.addEventListener("keydown", movePlayer);
-  document.addEventListener("keydown", shootBullet);
-
+  addMovePlayerAndShootBulletEvents()
   const timerController = {
     intervalId: setInterval(gameTimeCounter, 1000), // start the game timer
     isPaused: false,
